a217769fec6f59ff1bbaee125a1b593026973124
Kai <kai.schuleit@hotmail.de>
2025-05-14 16:01:04 -0400

    finish all

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -21,7 +21,7 @@ public_users.post("/register", (req,res) => {
 });
 
 // Get the book list available in the shop
-public_users.get('/', async(req, res) {
+public_users.get('/', async function(req, res) {
   //Write your code here
   try{
     const getBooks = async() => books;
@@ -34,7 +34,7 @@ public_users.get('/', async(req, res) {
 });
 
 // Get book details based on ISBN
-public_users.get('/isbn/:isbn',function (req, res) {
+public_users.get('/isbn/:isbn',async function (req, res) {
   //Write your code here
   const isbn = req.params.isbn;
   try{
@@ -47,7 +47,7 @@ public_users.get('/isbn/:isbn',function (req, res) {
  });
   
 // Get book details based on author
-public_users.get('/author/:author',function (req, res) {
+public_users.get('/author/:author',async function (req, res) {
   //Write your code here
   const author = req.params.author;
   try{
@@ -67,15 +67,26 @@ public_users.get('/author/:author',function (req, res) {
 });
 
 // Get all books based on title
-public_users.get('/title/:title',function (req, res) {
+public_users.get('/title/:title', async function (req, res) {
   //Write your code here
   const title = req.params.title;
-  for(const[key, value] of Object.entries(books)){
-    if(value.title.toLowerCase() === title.toLowerCase()){
-        return res.send(value);
+  try{
+    const findBookByTitle = async(a) => {
+       for(const[key, value] of Object.entries(books)){
+            if(value.title.toLowerCase() === a.toLowerCase()){
+                return value;
+            }
+        } 
+    } 
+    const book = await findBookByTitle(title);
+    if(book){
+        return res.send(book);
+    }else{
+        return res.status(403).json({message: "Book not found"});
     }
+  }catch(err){
+        return res.status(403).json({message: "Book not found", error: err});
   }
-  try{}
 });
 
 //  Get book review
