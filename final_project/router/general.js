fab79ed5f070b5ef1f92029be6de1489134c92a1
Kai <kai.schuleit@hotmail.de>
2025-05-14 14:38:14 -0400

    final0

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -21,28 +21,49 @@ public_users.post("/register", (req,res) => {
 });
 
 // Get the book list available in the shop
-public_users.get('/',function (req, res) {
+public_users.get('/', async(req, res) {
   //Write your code here
-  return res.send(JSON.stringify(books, null, 4));
+  try{
+    const getBooks = async() => books;
+    const bookList = await getBooks();
+    return res.send(JSON.stringify(bookList, null, 4));
+  }catch(err){
+    res.status(500).json({ message: "Error fetching books", error: err });
+  }
+  //return res.send(JSON.stringify(books, null, 4));
 });
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
   //Write your code here
-  const isbn = req.params.isbn
-  return res.send(books[isbn]);
-  
+  const isbn = req.params.isbn;
+  try{
+    const searchedBook = async()=> books[isbn];
+
+    return res.send(await searchedBook());
+  }catch(err){
+    res.status(403).json({ message: "Error fetching books", error: err });
+  }
  });
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
   //Write your code here
   const author = req.params.author;
-  for(const[key, value] of Object.entries(books)){
-    if(value.author.toLowerCase() === author.toLowerCase()){
-        return res.send(value);
+  try{
+    const bookByAuthor = async(a)=> {
+        for(const[key, value] of Object.entries(books)){
+            if(value.author.toLowerCase() === a.toLowerCase()){
+                return value;
+            }   
+        }
     }
+    const book = await bookByAuthor();
+    return res.send(book);
+  }catch(err){
+    return res.status(403).json({message: "Book not found", error: err});
   }
+  
 });
 
 // Get all books based on title
@@ -54,6 +75,7 @@ public_users.get('/title/:title',function (req, res) {
         return res.send(value);
     }
   }
+  try{}
 });
 
 //  Get book review
